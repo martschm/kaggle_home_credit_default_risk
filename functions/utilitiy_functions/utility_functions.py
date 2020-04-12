a35397ac151ca803150c6d9cf389eeb9d8645ae1
Martin Schmidt <schmidtmartin1992@outlook.de>
2020-04-12 02:04:58 +0200

    raw data preparation finished

---
+++ functions/utilitiy_functions/utility_functions.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def print_unique_char_values(pd_series, relative=True):
+    if relative:
+        print((round(pd_series.value_counts()/pd_series.shape[0]*100,1)).sort_values(ascending=False),"\n")
+    else:
+        print(pd_series.value_counts().sort_values(ascending=False),"\n")
